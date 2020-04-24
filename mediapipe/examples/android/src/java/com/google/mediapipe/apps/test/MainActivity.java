4b8cf631dfcf8ab27243641b2b019edad00c7871
H1Gdev <h1g.z.ouroboros+github@gmail.com>
2020-04-24 14:26:21 +0900

    [Android]Test apk.
    $ APP=test
    $ APPLICATION_PATH=//mediapipe/examples/android/src/java/com/google/mediapipe/apps/$APP
    [mobile-install]
    $ bazel mobile-install --compilation_mode opt --config=android_arm64 $APPLICATION_PATH --start_app
    [build and install]
    $ bazel build --compilation_mode opt --config=android_arm64 $APPLICATION_PATH
    $ adb install bazel-bin/$APPLICATION_PATH/$APP.apk

---
+++ mediapipe/examples/android/src/java/com/google/mediapipe/apps/test/MainActivity.java
@@ -0,0 +1,12 @@
+package com.google.mediapipe.apps.test;
+
+import android.os.Bundle;
+
+public class MainActivity extends com.google.mediapipe.apps.basic.MainActivity {
+  private static final String TAG = "MainActivity";
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
+}
