1b911388cc7a08562d49680b73f25f375a9863e6
H1Gdev <h1g.z.ouroboros+github@gmail.com>
2020-06-09 10:21:12 +0900

    [Desktop]Test binary.
    $ export GLOG_logtostderr=1
    [run]
    $ bazel run --compilation_mode dbg //mediapipe/examples/desktop/test:test_desktop
    [build and execute]
    $ bazel build --compilation_mode dbg //mediapipe/examples/desktop/test:test_desktop
    $ ./bazel-bin/mediapipe/examples/desktop/test/test_desktop

---
+++ mediapipe/examples/desktop/test/test_desktop.cc
@@ -0,0 +1,58 @@
+#include "mediapipe/framework/calculator_graph.h"
+#include "mediapipe/framework/port/logging.h"
+#include "mediapipe/framework/port/parse_text_proto.h"
+#include "mediapipe/framework/port/status.h"
+
+namespace mediapipe {
+
+::mediapipe::Status TestHelloWorld() {
+  // https://github.com/google/mediapipe/blob/v0.7.5/mediapipe/framework/calculator.proto#L218
+  CalculatorGraphConfig config = ParseTextProtoOrDie<CalculatorGraphConfig>(R"(
+    input_stream: "in"
+    output_stream: "out"
+    node {
+      calculator: "TestCalculator"
+      input_stream: "in"
+      output_stream: "out"
+      node_options: {
+        [type.googleapis.com/mediapipe.TestCalculatorOptions] {
+          test_name: "test"
+        }
+      }
+    }
+  )");
+
+  CalculatorGraph graph;
+  MP_RETURN_IF_ERROR(graph.Initialize(config));
+  ASSIGN_OR_RETURN(OutputStreamPoller poller, graph.AddOutputStreamPoller("out"));
+  MP_RETURN_IF_ERROR(graph.StartRun({}));
+
+  for (auto i = 0; i < 10; ++i) {
+    // make Packet.
+    // Packet is chain style.
+    auto packet = MakePacket<std::string>("Hello World!").At(Timestamp(i));
+    MP_RETURN_IF_ERROR(graph.AddPacketToInputStream("in", packet));
+  }
+
+  // Close the input stream "in".
+  MP_RETURN_IF_ERROR(graph.CloseInputStream("in"));
+
+  // Get the output packets std::string.
+  mediapipe::Packet packet;
+  while (poller.Next(&packet)) {
+    // Check packet type.
+    if (packet.ValidateAsType<std::string>().ok())
+      // Output Log.
+      LOG(INFO) << packet.Get<std::string>();
+  }
+  return graph.WaitUntilDone();
+}
+}  // namespace mediapipe
+
+int main(int argc, char** argv) {
+  // Use glog.
+  google::InitGoogleLogging(argv[0]);
+
+  CHECK(mediapipe::TestHelloWorld().ok());
+  return 0;
+}
