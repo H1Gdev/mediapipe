6486d7fbd6fdf11ea6f948f8c4deb276baa7f9af
H1Gdev <h1g.z.ouroboros+github@gmail.com>
2020-09-08 16:30:07 +0900

    [Desktop]Threading.
    - Executor, ExecutorConfig
    - ThreadPoolExecutor, ThreadPoolExecutorOptions

--- mediapipe/examples/desktop/test/test_desktop.cc
+++ mediapipe/examples/desktop/test/test_desktop.cc
@@ -90,6 +90,140 @@ namespace mediapipe {
 
   return ::mediapipe::OkStatus();
 }
+
+::mediapipe::Status TestThreading() {
+#if 0
+  // use num_threads.
+  CalculatorGraphConfig config = ParseTextProtoOrDie<CalculatorGraphConfig>(R"(
+    num_threads: 3
+
+    input_stream: "in"
+    output_stream: "out"
+    output_stream: "out1"
+    node {
+      calculator: "TestCalculator"
+      input_stream: "in"
+      output_stream: "out"
+    }
+    node {
+      calculator: "TestCalculator"
+      input_stream: "in"
+      output_stream: "out1"
+    }
+  )");
+
+#if 0
+  // Setting in code.
+  config.set_num_threads(5);
+#endif
+#elif 0
+  // use executor for default.
+  CalculatorGraphConfig config = ParseTextProtoOrDie<CalculatorGraphConfig>(R"(
+    # https://github.com/google/mediapipe/blob/master/mediapipe/framework/calculator.proto#L35
+    executor {
+      # https://github.com/google/mediapipe/blob/master/mediapipe/framework/executor.h
+      # https://github.com/google/mediapipe/blob/master/mediapipe/framework/thread_pool_executor.h
+      type: "ThreadPoolExecutor"
+      options {
+        # https://github.com/google/mediapipe/blob/master/mediapipe/framework/thread_pool_executor.proto
+        [mediapipe.ThreadPoolExecutorOptions.ext] {
+          num_threads: 2
+        }
+      }
+      # "ApplicationThreadExecutor" not work...
+      #type: "ApplicationThreadExecutor"
+    }
+
+    input_stream: "in"
+    output_stream: "out"
+    output_stream: "out1"
+    node {
+      calculator: "TestCalculator"
+      input_stream: "in"
+      output_stream: "out"
+    }
+    node {
+      calculator: "TestCalculator"
+      input_stream: "in"
+      output_stream: "out1"
+    }
+  )");
+
+#if 0
+  // Setting in code.
+  auto executor = config.add_executor();
+  executor->set_type("ThreadPoolExecutor");
+  auto options = executor->mutable_options();
+  options->MutableExtension(ThreadPoolExecutorOptions::ext)->set_num_threads(2);
+#endif
+#elif 1
+  // use executor(s).
+  CalculatorGraphConfig config = ParseTextProtoOrDie<CalculatorGraphConfig>(R"(
+    executor {
+      # reserved: "default", "gpu"
+      name: "test0"
+      type: "ThreadPoolExecutor"
+      options {
+        [mediapipe.ThreadPoolExecutorOptions.ext] {
+          num_threads: 2
+          require_processor_performance: LOW
+        }
+      }
+    }
+    executor {
+      name: "test1"
+      type: "ThreadPoolExecutor"
+      options {
+        [mediapipe.ThreadPoolExecutorOptions.ext] {
+          num_threads: 2
+        }
+      }
+    }
+
+    input_stream: "in"
+    output_stream: "out"
+    output_stream: "out1"
+    node {
+      calculator: "TestCalculator"
+      input_stream: "in"
+      output_stream: "out"
+      executor: "test0"
+    }
+    node {
+      calculator: "TestCalculator"
+      input_stream: "in"
+      output_stream: "out1"
+      executor: "test1"
+    }
+  )");
+#endif
+
+  LOG(INFO) << "[Default]num_threads=" << config.num_threads();
+
+  LOG(INFO) << "[Executor]" << config.executor().size();
+  for (const auto& executor : config.executor()) {
+    LOG(INFO) << "  " << executor.name() << "(" << executor.type() << ")";
+  }
+
+  CalculatorGraph graph;
+  MP_RETURN_IF_ERROR(graph.Initialize(config));
+  ASSIGN_OR_RETURN(OutputStreamPoller poller, graph.AddOutputStreamPoller("out"));
+  MP_RETURN_IF_ERROR(graph.StartRun({}));
+
+  for (auto i = 0; i < 4; ++i) {
+    auto packet = MakePacket<std::string>("Threading.").At(Timestamp(i));
+    MP_RETURN_IF_ERROR(graph.AddPacketToInputStream("in", packet));
+  }
+
+  MP_RETURN_IF_ERROR(graph.CloseInputStream("in"));
+
+  mediapipe::Packet packet;
+  while (poller.Next(&packet)) {
+    if (packet.ValidateAsType<std::string>().ok())
+      LOG(INFO) << packet.Get<std::string>();
+  }
+  return graph.WaitUntilDone();
+}
 }  // namespace mediapipe
 
 int main(int argc, char** argv) {
@@ -117,5 +251,9 @@ int main(int argc, char** argv) {
   LOG(INFO) << "[START]Test Side Packet";
   CHECK(mediapipe::TestSidePacket().ok());
   LOG(INFO) << "[END]Test Side Packet";
+
+  LOG(INFO) << "[START]Test Threading";
+  CHECK(mediapipe::TestThreading().ok());
+  LOG(INFO) << "[END]Test Threading";
   return 0;
 }
