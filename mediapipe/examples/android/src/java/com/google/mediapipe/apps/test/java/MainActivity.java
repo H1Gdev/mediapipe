a1a288840d938acaf6147037b536105cbf53e24e
H1Gdev <h1g.z.ouroboros+github@gmail.com>
2020-10-31 07:25:16 +0900

    [Android]Get Detection(ProtocolBuffers type) in app.

--- mediapipe/examples/android/src/java/com/google/mediapipe/apps/test/java/MainActivity.java
+++ mediapipe/examples/android/src/java/com/google/mediapipe/apps/test/java/MainActivity.java
@@ -3,18 +3,29 @@ package com.google.mediapipe.apps.test.java;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.mediapipe.formats.proto.DetectionProto;
 import com.google.mediapipe.framework.AndroidPacketCreator;
 import com.google.mediapipe.framework.Packet;
 import com.google.mediapipe.framework.PacketGetter;
+import com.google.mediapipe.framework.ProtoUtil;
+import com.google.protobuf.InvalidProtocolBufferException;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class MainActivity extends com.google.mediapipe.apps.basic.MainActivity {
   private static final String TAG = "MainActivity";
 
   private static final String OUTPUT_VALUE_STREAM_NAME = "output_value";
+  private static final String DETECTION_STREAM_NAME = "detection";
+  private static final String DETECTIONS_STREAM_NAME = "detections";
+
+  static {
+      // https://github.com/google/mediapipe/issues/1013
+      ProtoUtil.registerTypeName(DetectionProto.Detection.class, "mediapipe.Detection");
+  }
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -37,5 +48,24 @@ public class MainActivity extends com.google.mediapipe.apps.basic.MainActivity {
                 Log.v(TAG, "output_value is " + Arrays.toString(outputValue) + " (" + packet.getTimestamp() + ")");
               });
     }
+
+    // for Detection
+    processor.addPacketCallback(
+            DETECTION_STREAM_NAME,
+            (packet -> {
+                // not called Callback if Packet is empty.
+                try {
+                    DetectionProto.Detection detection = PacketGetter.getProto(packet, DetectionProto.Detection.class);
+                    Log.d(TAG, "Detection is " + detection);
+                } catch (InvalidProtocolBufferException e) {
+                    Log.e(TAG, "PacketGetter.getProto()", e);
+                }
+            }));
+      processor.addPacketCallback(
+              DETECTIONS_STREAM_NAME,
+              (packet -> {
+                  List<DetectionProto.Detection> detections = PacketGetter.getProtoVector(packet, DetectionProto.Detection.parser());
+                  Log.d(TAG, "Detections is " + detections);
+              }));
   }
 }
--- mediapipe/examples/android/src/java/com/google/mediapipe/apps/test/kotlin/MainActivity.kt
+++ mediapipe/examples/android/src/java/com/google/mediapipe/apps/test/kotlin/MainActivity.kt
@@ -3,16 +3,25 @@ package com.google.mediapipe.apps.test.kotlin
 import android.os.Bundle
 import android.util.Log
 import com.google.mediapipe.apps.basic.MainActivity
+import com.google.mediapipe.formats.proto.DetectionProto
 import com.google.mediapipe.framework.Packet
 import com.google.mediapipe.framework.PacketGetter
+import com.google.mediapipe.framework.ProtoUtil
+import com.google.protobuf.InvalidProtocolBufferException
 import java.util.Arrays
 import java.util.HashMap
 
 class MainActivity : MainActivity() {
 
   companion object {
+    init {
+      // https://github.com/google/mediapipe/issues/1013
+      ProtoUtil.registerTypeName<DetectionProto.Detection>(DetectionProto.Detection::class.java, "mediapipe.Detection")
+    }
     private const val TAG = "MainActivity"
     private const val OUTPUT_VALUE_STREAM_NAME = "output_value"
+    private const val DETECTION_STREAM_NAME = "detection"
+    private const val DETECTIONS_STREAM_NAME = "detections"
   }
 
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,6 +41,25 @@ class MainActivity : MainActivity() {
         val outputValue = PacketGetter.getFloat32Vector(packet)
         Log.v(TAG, "output_value is " + Arrays.toString(outputValue) + " (" + packet.timestamp + ")")
       }
+    }
+
+    // for Detection
+    processor.addPacketCallback(
+            DETECTION_STREAM_NAME
+    ) { packet: Packet? ->
+      // not called Callback if Packet is empty.
+      try {
+        val detection: DetectionProto.Detection = PacketGetter.getProto(packet, DetectionProto.Detection::class.java)
+        Log.d(TAG, "Detection is $detection")
+      } catch (e: InvalidProtocolBufferException) {
+        Log.e(TAG, "PacketGetter.getProto()", e)
+      }
+    }
+    processor.addPacketCallback(
+            DETECTIONS_STREAM_NAME
+    ) { packet: Packet? ->
+      val detections: List<DetectionProto.Detection> = PacketGetter.getProtoVector(packet, DetectionProto.Detection.parser())
+      Log.d(TAG, "Detections is $detections")
     }
   }
 }
